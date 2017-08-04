c66c6f001c4e1b9007957eadbd89b76725a76e3a
Arun Sivakumar <arunsivakumar@live.com>
2017-08-04 11:18:22 -0600

    Add Search Store

---
+++ MovieHawk/Store/SearchStore.swift
@@ -0,0 +1,14 @@
+//
+//  SearchStore.swift
+//  MovieHawk
+//
+//  Created by Lakshmi on 8/4/17.
+//  Copyright © 2017 com.arunsivakumar. All rights reserved.
+//
+
+import Foundation
+
+class SearchStore{
+    
+    
+}
