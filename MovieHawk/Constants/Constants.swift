76897c02f5948b2753912e3e951b4d5d77ed2dbb
Arun Sivakumar <arunsivakumar@live.com>
2017-08-08 15:01:46 -0600

    Add constants for colors

--- MovieHawk/Constants/Constants.swift
+++ MovieHawk/Constants/Constants.swift
@@ -14,4 +14,8 @@ struct Constants{
     static let navBarColor = UIColor.darkGray
     static let defaultFontNavigation = "HelveticaNeue-thin"
     static let defaultFontSizeNavigation:CGFloat = 18
+    
+    static let  watchButtonDisabledColor = UIColor.darkGray
+    static let  watchButtonEnabledColor = UIColor(red: 231/255, green: 187/255, blue: 78/255, alpha: 1.0)
+
 }
